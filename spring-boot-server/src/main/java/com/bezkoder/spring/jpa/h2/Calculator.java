9a22b1709635e6afa15e6aa21c65bac787d705c2
Moni361989 <monica.ocampom@perficient.com>
2023-04-16 19:36:27 -0500

    Tarea 1

---
+++ spring-boot-server/src/main/java/com/bezkoder/spring/jpa/h2/Calculator.java
@@ -0,0 +1,17 @@
+package com.bezkoder.spring.jpa.h2;
+
+public class Calculator {
+
+    public static void main(String args[]) {
+        System.out.println(suma(785, 967, 325));
+        System.out.println(division(643, 82));
+    }
+
+    public static Integer suma(int num1, int num2, int num3) {
+        return num1 + num2 + num3;
+    }
+
+    public static Double division(double num1, double num2) {
+        return num1 / num2;
+    }
+}
